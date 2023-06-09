abd65bc353f99a372d66932aabb76b00cd40865c
Yemima Sutanto <64170347+pootreth@users.noreply.github.com>
2023-06-10 03:53:53 +0700

    add create a post modal

---
+++ src/components/common/PostUpdate/index.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import ModalComponent from "../Modal";
+import "./index.scss";
+
+export default function PostStatus() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [status, setStatus] = useState("");
+  return (
+    <div className="post-status-main">
+      <div className="post-status">
+        <button className="open-post-modal" onClick={() => setModalOpen(true)}>
+          Start a post
+        </button>
+      </div>
+
+      <ModalComponent
+        setStatus={setStatus}
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+        status={status}
+      />
+    </div>
+  );
+}
--- src/components/common/Topbar/index.jsx
+++ src/components/common/Topbar/index.jsx
@@ -1,6 +1,6 @@
 import "./index.scss";
 import linkedinLogo from "../../../assets/linkedinLogo.png";
-import user from '../../../assets/userIcon.png'
+import user from "../../../assets/userIcon.png";
 import {
   AiOutlineBell,
   AiOutlineHome,
@@ -8,16 +8,29 @@ import {
   AiOutlineSearch,
   AiOutlineUserSwitch,
 } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 import { BsBriefcase } from "react-icons/bs";
 
 export default function Topbar() {
+  let navigate = useNavigate();
+  const goToRoute = (route) => {
+    navigate(route);
+  };
   return (
     <div className="topbar-main">
       <img className="linkedin-logo" src={linkedinLogo} alt="linkedinLogo" />
       <div className="react-icons">
         <AiOutlineSearch size={30} className="react-icon" />
-        <AiOutlineHome size={30} className="react-icon" />
-        <AiOutlineUserSwitch size={30} className="react-icon" />
+        <AiOutlineHome
+          size={30}
+          className="react-icon"
+          onClick={() => goToRoute("/home")}
+        />
+        <AiOutlineUserSwitch
+          size={30}
+          className="react-icon"
+          onClick={() => goToRoute("/profile")}
+        />
         <BsBriefcase size={30} className="react-icon" />
         <AiOutlineMessage size={30} className="react-icon" />
         <AiOutlineBell size={30} className="react-icon" />
