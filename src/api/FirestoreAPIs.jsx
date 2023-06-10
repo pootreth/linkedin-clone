73aa26120bc647e8cd85865fb8651582f0b494ff
Yemima Sutanto <64170347+pootreth@users.noreply.github.com>
2023-06-11 04:04:34 +0700

    init post card

--- src/api/FirestoreAPIs.jsx
+++ src/api/FirestoreAPIs.jsx
@@ -1,16 +1,28 @@
 import { firestore } from "../firebaseConfig";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { toast } from "react-toastify";
 
 let dbRef = collection(firestore, "posts");
+
 export const postStatus = (status) => {
   let object = {
     status: status,
   };
-  addDoc(dbRef, object).then((res) => {
-    toast.success("Document has been added successfully");
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+  addDoc(dbRef, object)
+    .then((res) => {
+      toast.success("Document has been added successfully");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const getStatus = (setAllStatus) => {
+  onSnapshot(dbRef, (response) => {
+    setAllStatus(
+      response.docs.map((docs) => {
+        return { ...docs.data(), id: docs.id };
+      })
+    );
+  });
 };
--- src/components/common/PostUpdate/index.jsx
+++ src/components/common/PostUpdate/index.jsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
-import { postStatus } from "../../../api/FirestoreAPIs";
+import { useState, useMemo } from "react";
+import { postStatus, getStatus } from "../../../api/FirestoreAPIs";
 import ModalComponent from "../Modal";
+import PostsCard from "../PostsCard";
 import "./index.scss";
 
 export default function PostStatus() {
   const [modalOpen, setModalOpen] = useState(false);
   const [status, setStatus] = useState("");
+  const [allStatus, setAllStatus] = useState([]);
   const sendStatus = async () => {
     await postStatus(status);
     await setModalOpen(false);
     await setStatus("");
-  }
+  };
+
+  useMemo(() => {
+    getStatus(setAllStatus);
+  }, []);
+
   return (
     <div className="post-status-main">
       <div className="post-status">
@@ -26,6 +33,12 @@ export default function PostStatus() {
         status={status}
         sendStatus={sendStatus}
       />
+
+      <div>
+        {allStatus.map((posts) => {
+          return <PostsCard posts={posts} />;
+        })}
+      </div>
     </div>
   );
 }
